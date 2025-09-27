ecb6c2fec140da17fc7e9732dbe9abe9332ad92c
arpit-shukla-13 <arpitshukla702@gmail.com>
2025-09-27 23:35:05 +0530

    update

--- frontend/src/app/home/HeroBullets.jsx
+++ frontend/src/app/home/HeroBullets.jsx
@@ -1,7 +1,6 @@
 'use client'
 import { Image, Container, Title, Button, Group, Text, List, ThemeIcon, rem } from '@mantine/core';
 import { IconCheck } from '@tabler/icons-react';
-// import image from '/image.svg';
 import classes from './HeroBullets.module.css';
 import Link from 'next/link';
 
